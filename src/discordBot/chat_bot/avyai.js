41b18616541588841142a12466d004ee7cf4b9e5
Airzy <120424474+Air-zy@users.noreply.github.com>
2026-04-25 01:55:19 -0600

    again

--- src/discordBot/chat_bot/avyai.js
+++ src/discordBot/chat_bot/avyai.js
@@ -75,10 +75,9 @@ async function generate(inputData, { onDelta, onFinal, onError } = {}) {
 
 //
 
-const llama3Tokenizer = require('llama3-tokenizer-js/bundle/commonjs-llama3-tokenizer-with-baked-data.cjs');
+const llama3TokenizerMODULE = require('llama3-tokenizer-js/bundle/commonjs-llama3-tokenizer-with-baked-data.cjs');
+const llama3Tokenizer = llama3TokenizerMODULE.llama3Tokenizer;
 console.log("[TEST] llama3Tokenizer:", llama3Tokenizer);
-console.log(llama3Tokenizer.encode)
-console.log(llama3Tokenizer.decode)
 
 function buildLogitBiasFromHistory(history, bias = -1, minCount = 1) {
     console.log("Building logit bias from history...", llama3Tokenizer);
